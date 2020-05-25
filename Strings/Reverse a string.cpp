287952d0c9ff16074122bc3c24b32f0e38fd8428
mansikagrawal <mansiagrawal2103@gmail.com>
2020-05-25 17:32:33 +0530

    String Problems

---
+++ Strings/Reverse a string.cpp
@@ -0,0 +1,25 @@
+#include<bita/stdc++.h>
+using namespace std;
+int main() {
+  // code
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    string temp = "";
+    for (int i = 0; i < s.length(); i++) {
+      if (s[i] != '.') {
+        temp += s[i];
+      } else {
+        reverse(temp.begin(), temp.end());
+        cout << temp << ".";
+        temp = "";
+      }
+    }
+    reverse(temp.begin(), temp.end());
+    cout << temp << endl;
+  }
+  return 0;
+}
