51e11746156a4d6d234c31f9b8b237fd53185cc3
mansikagrawal <mansiagrawal2103@gmail.com>
2020-06-02 01:03:16 +0530

    Update Fast modulo using bitmasks.cpp

--- Number theory - 3/Fast modulo using bitmasks.cpp
+++ Number theory - 3/Fast modulo using bitmasks.cpp
@@ -1,27 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 #define ll long long
 
-ll fastmod(ll a , ll b, ll m)
+ll fastmod(ll a, ll b, ll m)
 {
 
-    ll res =1 ;
-    while(b>0)
-    {
-        if(b&1)
-        {
-            res = (res*a) % m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) {
+            res = (res * a) % m;
         }
-        a=(a*a) % m;
-        b=b>>1;
+        a = (a * a) % m;
+        b = b >> 1;
     }
     return res;
 }
 int main()
 {
-   cout<< fastmod(5,3,100);
+    cout << fastmod(5, 3, 100);
     return 0;
-
-
 }
