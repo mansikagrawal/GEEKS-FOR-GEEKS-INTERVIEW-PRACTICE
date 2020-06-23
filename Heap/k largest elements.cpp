78cd7a2fa8c426c988ad1d29b1196d386e5bcfc4
mansikagrawal <mansiagrawal2103@gmail.com>
2020-06-23 23:18:28 +0530

    Create k largest elements.cpp

---
+++ Heap/k largest elements.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+vector<int> kLargest(int arr[], int n, int k)
+{
+    // code here
+    vector<int> v;
+    priority_queue<int> pq(arr, arr+n);
+    while(k>0)
+    {
+      k--;
+      v.push_back(pq.top());
+      pq.pop();
+    }
+    return v;
+}
+
+
+vector<int> kLargest(int arr[], int n, int k);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        int arr[n];
+        for(int i = 0; i < n;i++)
+            cin>>arr[i];
+
+        vector<int> result = kLargest(arr, n, k);
+        for (int i = 0; i < result.size(); ++i)
+            cout<<result[i]<<" ";
+        cout << endl;
+
+    }
+    return 0;
+}
