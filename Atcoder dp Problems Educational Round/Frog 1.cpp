18546b064f202427fefe13035aaa48fc1a952fc9
Mansi Agrawal <46123053+mansikagrawal@users.noreply.github.com>
2020-08-23 00:45:19 +0530

    Problem A

--- Atcoder dp Problems Educational Round/Frog 1.cpp
+++ Atcoder dp Problems Educational Round/Frog 1.cpp
@@ -5,29 +5,24 @@
 using namespace std;
 int solve(int arr[], int n)
 {
-    int dp[n+1];// minimum cost to reach the ith stone 
-    dp[1] = 0 ;
-    for(int i=2;i<=n;i++)
-    {
-        int option1= abs(arr[i] -arr[i-1]) + dp[i-1];
-        int option2 = (i==2)? inf :abs(arr[i] - arr[i-2]) + dp[i-2];
-        dp[i] = min(option2,option1);
-        
+    int dp[n + 1]; // minimum cost to reach the ith stone
+    dp[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        int option1 = abs(arr[i] - arr[i - 1]) + dp[i - 1];
+        int option2 = (i == 2) ? inf : abs(arr[i] - arr[i - 2]) + dp[i - 2];
+        dp[i] = min(option2, option1);
     }
-    
-    
+
     return dp[n];
-    
-    
-    
 }
 
-int main() {
-	int n;
-	cin>>n;
-	int arr[n+1];
-	for(int i=1;i<=n;i++)
-	cin>>arr[i];
-	cout << solve(arr,n);
-	return 0;
-}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n + 1];
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+    cout << solve(arr, n);
+    return 0;
+}iuuuuuu
